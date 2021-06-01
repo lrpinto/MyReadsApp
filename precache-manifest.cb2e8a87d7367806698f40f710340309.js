8dc46ce2a4018e53e3fdfde79a181c97609d5662
Luisa Pinto <luisa.pinto@footasylum.com>
2021-06-01 12:23:34 +0100

    Updates

---
+++ precache-manifest.cb2e8a87d7367806698f40f710340309.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "86a84e05c4382213010a",
+    "url": "/lrpinto-reactnd-project-myreads/static/css/main.0bb6cc37.chunk.css"
+  },
+  {
+    "revision": "86a84e05c4382213010a",
+    "url": "/lrpinto-reactnd-project-myreads/static/js/main.86a84e05.chunk.js"
+  },
+  {
+    "revision": "d2f3dec24e596f779d99",
+    "url": "/lrpinto-reactnd-project-myreads/static/js/1.d2f3dec2.chunk.js"
+  },
+  {
+    "revision": "34d014e684b272d3e645",
+    "url": "/lrpinto-reactnd-project-myreads/static/js/runtime~main.34d014e6.js"
+  },
+  {
+    "revision": "bb9363b86b02777d23523851826414e0",
+    "url": "/lrpinto-reactnd-project-myreads/static/media/add.bb9363b8.svg"
+  },
+  {
+    "revision": "3687aa988bec28f17ae39153db1646c3",
+    "url": "/lrpinto-reactnd-project-myreads/static/media/arrow-back.3687aa98.svg"
+  },
+  {
+    "revision": "78c6f53b2342643639fe1cbaf171bf47",
+    "url": "/lrpinto-reactnd-project-myreads/static/media/arrow-drop-down.78c6f53b.svg"
+  },
+  {
+    "revision": "7868f639a4380d0e48bb568f3ee1fe25",
+    "url": "/lrpinto-reactnd-project-myreads/index.html"
+  }
+];
